138fba038bbee8cf5eabf8e1a573f9d738394e69
Your Name <you@example.com>
2021-08-28 17:46:09 +0700

    update

--- src/Bai1_3.java
+++ src/Bai1_3.java
@@ -3,28 +3,15 @@ import java.util.Scanner;
 import javax.xml.transform.Templates;
 
 public class Bai1_3 {
-	private int length; //kich thuoc mang
-	private int[] arr;
+	private int[] arr= {22, 4, 61, 3, 7, 9, 2};
 	private int index=0; //luu so phan tu cua mang phu (cau 3)
 	
 	public Bai1_3() {
-		Scanner scanner = new Scanner(System.in);
-		// TODO Auto-generated constructor stub
-		System.out.println("Nhap kich thuoc cua mang");
-		this.length= scanner.nextInt();
-		this.arr= new int[length];
+		
 	}
-	public void nhap() {
-		Scanner scanner = new Scanner(System.in);
-		for (int i = 0; i <length; i++) {
-			System.out.print("Nhap arr["+i+"]= ");
-			arr[i]= Integer.parseInt(scanner.nextLine());
-		}
-	}
-	
 	public void xuat() {
 		System.out.println("Cac phan tu cua mang:");
-		for (int i = 0; i <length; i++) {
+		for (int i = 0; i <arr.length; i++) {
 			System.out.print(arr[i]+ " ");
 		}
 		System.out.println("");
@@ -32,7 +19,7 @@ public class Bai1_3 {
 	
 	public int tongChan() {
 		int tong=0;
-		for (int i = 0; i < length; i+=2) {
+		for (int i = 0; i < arr.length; i+=2) {
 			tong+= arr[i];
 		}
 		return tong;
@@ -41,7 +28,7 @@ public class Bai1_3 {
 	public int viTriThu2(int x) {
 		int index=-1;
 		int check=0;
-		for (int i = 0; i < length; i++) {
+		for (int i = 0; i < arr.length; i++) {
 			if(arr[i] == x && check==1) {
 				return i;
 			}else if(arr[i] == x) {
@@ -52,8 +39,8 @@ public class Bai1_3 {
 	}
 	
 	public int[] boiCua2() {
-		int[] temp = new int[length];
-		for (int i = 0; i < length; i++) {
+		int[] temp = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
 			if(arr[i] %2 ==0) {
 				temp[index] = arr[i];
 				index++;
@@ -66,7 +53,6 @@ public class Bai1_3 {
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
 		Bai1_3 bai1_3 = new Bai1_3();
-		bai1_3.nhap();
 		bai1_3.xuat();
 		System.out.println("Tong cac phan tu o vi tri chan: "+ bai1_3.tongChan());
 		System.out.println("Nhap vao 1 so bat ki");
@@ -76,7 +62,7 @@ public class Bai1_3 {
 		}else {
 			System.out.println("khong tim thay vi tri xuat hien thu 2 cua " + x);
 		}
-		int[] temp= new int[bai1_3.length];
+		int[] temp= new int[bai1_3.arr.length];
 		temp=  bai1_3.boiCua2();
 		System.out.println("Cac phan tu la boi cua 2:");
 		for (int i = 0; i < bai1_3.index; i++) {
